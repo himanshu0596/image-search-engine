767c6266ef40f1cb30b2bcedec88164ae2d76509
Himanshu Singal <himanshu.singal@gds.ey.com>
2023-06-03 12:52:13 +0530

    implemented debouncing for search on every key stroke and moved header stying to separate css file

--- src/components/ImageSearch.js
+++ src/components/ImageSearch.js
@@ -1,29 +1,45 @@
 import React, {useState, useEffect } from 'react';
 import axios from 'axios';
 import InfiniteScroll from 'react-infinite-scroll-component';
+import Header from './header';
 
 function ImageSearch() { 
 // State variables
 const [query, setQuery] = useState('');
-const [images, setlmages] = useState ([]);
+const [images, setImages] = useState ([]);
 const [page, setPage] = useState (1);
 
 useEffect(() => {
 // Fetch initial images when the component mounts 
-fetchlmages();
+fetchImages();
 }, []);
 
-const fetchlmages = () => {
+useEffect(()=>{
+// to automatically search images when user stops typing
+
+// using debouncing to delay the execution of callback function and cleaning up the timer to prevent unnecessary api calls on every key press
+
+const delayDebounceFn = setTimeout(()=>{
+    if(query){
+        setImages ([]);
+        setPage(1); 
+        fetchImages();
+    }
+}, 500)
+
+return () => clearTimeout(delayDebounceFn)
+}, [query])
+
+const fetchImages = async () => {
     // Make a GET request to the Unsplash API 
-    axios.get(`https://api.unsplash.com/search/photos?query=${query}&page=${page}`, { 
+    await axios.get(`https://api.unsplash.com/search/photos?query=${query}&page=${page}`, { 
         headers: {
             Authorization: 'Client-ID leSfvNLvtPfJROqCppHy6sW9XwBeD3prf0WMmxEGYoA',
         },
     })
     .then(response => {
         // Update the images state with the new results
-        console.log(response, '1155351')
-        setlmages(prevlmages => [...prevlmages, ...response.data.results]);
+        setImages(prevlmages => [...prevlmages, ...response.data.results]);
         setPage(prevPage => prevPage+1 )
     })
     .catch(error => { console.log(error);
@@ -37,20 +53,17 @@ const handlelnputChange = event => {
 const handleSubmit = event => {
     event.preventDefault();
     // Clear existing images and fetch new ones based on the query 
-    setlmages ([]);
+    setImages ([]);
     setPage(1); 
-    fetchlmages();
+    fetchImages();
 };
 
 return (
     <div>
-        <form onSubmit={handleSubmit}> 
-            <input type="text" value={query} onChange={handlelnputChange} placeholder="Search images..." />
-            <button type="submit">Search</ button>
-        </form>
+        <Header handleSearchSubmit={handleSubmit} handleSearchChange={handlelnputChange} searchValue={query}></Header>
 
         <InfiniteScroll
-        dataLength={images.length} next={fetchlmages} hasMore={true}
+        dataLength={images.length} next={fetchImages} hasMore={true}
         loader={<h4>Loading..</h4>}
         >
             {
--- src/components/header.js
+++ src/components/header.js
@@ -1,61 +1,25 @@
-import React, { useState } from 'react';
+import React from 'react';
+import '../styles/header.css'
 
 const Header = (props) => {
 
   return (
-    <header style={headerStyle}>
-      <nav style={navStyle}>
-        <div style={logoStyle}>Your Logo</div>
-        <form onSubmit={props.handleSearchSubmit} style={formStyle}>
+    <header className="headerStyle">
+      <nav className="navStyle">
+        <div className="logoStyle">Your Logo</div>
+        <form onSubmit={props.handleSearchSubmit} className="formStyle">
           <input
             type="text"
             placeholder="Search..."
             value={props.searchValue}
             onChange={props.handleSearchChange}
-            style={inputStyle}
+            className="inputStyle"
           />
-          <button type="submit" style={buttonStyle}>Search</button>
+          <button type="submit" className="buttonStyle">Search</button>
         </form>
       </nav>
     </header>
   );
-};
-
-// Styles
-const headerStyle = {
-  backgroundColor: '#333',
-  padding: '10px 0',
-};
-
-const navStyle = {
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'space-between',
-  maxWidth: '900px',
-  margin: '0 auto',
-  color: '#fff',
-};
-
-const logoStyle = {
-  fontSize: '1.5rem',
-  fontWeight: 'bold',
-};
-
-const formStyle = {
-  display: 'flex',
-};
-
-const inputStyle = {
-  padding: '5px',
-  marginRight: '10px',
-};
-
-const buttonStyle = {
-  padding: '5px 10px',
-  backgroundColor: '#777',
-  color: '#fff',
-  border: 'none',
-  cursor: 'pointer',
 };
 
 export default Header;
